7aab6b5e2e2e60b3d1c9434307efd37d46464dba
gyk <123@163.com>
2023-01-02 13:34:31 +0800

    实现effect返回runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,25 +1,30 @@
 
+// 对象的依赖
 class ReactiveEffect {
+    // fn指的就是reactive()传入的函数
     private _fn: any;
     constructor(fn){
         this._fn = fn;
     }
 
+    // 执行reactive()传入的函数
     run(){
         activeEffect = this;
-        this._fn()
+        return this._fn()
     }
 }
+
+
+
 // 收集依赖（依赖就是指带fn的那个实例对象）
-// targetMap {
-//   target:depsMap
-//}
-// depsMap {
-//   key:dep
-//}
+
+// 设计数据结构：
+// 1.可能会存在多个对象需要追踪，所以必须要一个存放这些对象的容器targetMap
+// 2.一个对象可能会存在多个key需要追踪，所以必须要一个存放这些对象的key的依赖的容器depsMap
 const targetMap = new Map();
 export function track(target, key) {
     // target -> depsMap -> key
+    // 1.选择对象
     let depsMap = targetMap.get(target);
     // 初始化时创建depsMap放到targetMap
     if(!depsMap){
@@ -27,22 +32,20 @@ export function track(target, key) {
         targetMap.set(target, depsMap);
     }
 
+    // 2.选择对象的key对应存储的依赖
     let dep = depsMap.get(key)
     // 初始化时创建dep放到depsMap
     if(!dep){
+        // dep用来存储依赖
         dep = new Set();
         depsMap.set(key, dep)
     }
+    // 将依赖加入，实际存储的就是fn
     dep.add(activeEffect)
 }
 
+
 // 触发依赖
-// targetMap {
-//   target:depsMap
-//}
-// depsMap {
-//   key:dep
-//}
 export function trigger(target, key){
     let depsMap = targetMap.get(target);
     let dep = depsMap.get(key);
@@ -51,11 +54,17 @@ export function trigger(target, key){
     }
 }
 
+
+
 // effect包裹的内容被自动调用
 let activeEffect;
 export function effect(fn){
+    // 新建ReactiveEffect是为了使用面向对象编程
     const _effect = new ReactiveEffect(fn)
+    // 调用reactive()传入的函数
     _effect.run();
+
+    return _effect.run.bind(_effect);
 }
 
 
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -6,17 +6,21 @@ export function reactive(raw) {
     return new Proxy(raw,{
         // 当调用对象属性时自动触发
         get(target, key){
+            // Reflect从target中获取对应key的值
             const res = Reflect.get(target, key)
             // 依赖收集
             track(target, key)
+            // 返回获取到的属性值
             return res;
         },
 
         // 当设置对象属性时自动触发
         set(target, key, value){
+            // Reflect在target上获取对应key的值
             const res = Reflect.set(target, key, value);
             // 触发依赖
             trigger(target, key)
+            // 返回设置好的属性值
             return res
         }
     });
