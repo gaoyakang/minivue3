c1cefe0e50cc24cba1262abbbed47bb2c22394c5
gyk <123@163.com>
2023-01-02 17:23:58 +0800

    实现readonly功能

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -25,3 +25,22 @@ export function reactive(raw) {
         }
     });
 }
+
+
+export function readonly(raw){
+    // 通过proxy劫持原对象，get/set操作对象属性
+    return new Proxy(raw,{
+        // 当调用对象属性时自动触发
+        get(target, key){
+            // Reflect从target中获取对应key的值
+            const res = Reflect.get(target, key)
+            // 返回获取到的属性值
+            return res;
+        },
+
+        // 当设置对象属性时自动触发
+        set(target, key, value){
+            return true;
+        }
+    });
+}
